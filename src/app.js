08d7538ed5f61cea6318d1d40e10420a8691a290
sabrinaaileen <115700137+sabrinaaileen@users.noreply.github.com>
2022-10-21 11:12:51 +0200

    Adding date and time function

--- src/app.js
+++ src/app.js
@@ -1,3 +1,47 @@
+function showDate() {
+  let date = new Date();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Sunday",
+  ];
+  let day = days[date.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+  let currentDay = date.getDate();
+  let year = date.getFullYear();
+  let actualDate = document.querySelector("#date-today");
+  actualDate.innerHTML = `${day}, ${currentDay}. ${month} ${year}`;
+  let actualTime = document.querySelector("#time-today");
+  actualTime.innerHTML = `${hours}:${minutes}`;
+}
+showDate();
+
 function showTemperature(response) {
   console.log(response.data);
   let temperature = document.querySelector(".actualTemp");
