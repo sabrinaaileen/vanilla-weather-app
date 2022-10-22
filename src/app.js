863126c6522cc1bf979d82bc8cb8f33964c838af
sabrinaaileen <115700137+sabrinaaileen@users.noreply.github.com>
2022-10-22 13:40:24 +0200

    First steps of search engine

--- src/app.js
+++ src/app.js
@@ -45,14 +45,25 @@ showDate();
 function showTemperature(response) {
   console.log(response.data);
   let temperature = document.querySelector(".actualTemp");
+  let currentCity = document.querySelector("#city");
   let description = document.querySelector("#description");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
+
   temperature.innerHTML = Math.round(response.data.temperature.current);
+  currentCity.innerHTML = response.data.city;
   description.innerHTML = response.data.condition.description;
   humidity.innerHTML = response.data.temperature.humidity;
   wind.innerHTML = Math.round(response.data.wind.speed);
 }
+
+function searchCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#input-city");
+  search(city);
+}
+let searching = document.querySelector("");
+
 let city = "Munich";
 let apiKey = "02060cacd430ctof7d20b656741fc18d";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
